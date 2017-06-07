92b893d0051842c2916162433a3fa32375cdad53
rinoshaparvin <rinosha14@gmail.com>
2017-06-07 18:03:11 +0530

    Create qqq.java

---
+++ qqq.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class qqq
+{
+public static void main(String args[])
+{
+Scanner sca=new Scanner(System.in);
+int a=sca.nextInt();
+if(a<0)
+{
+System.out.println("neg");
+}
+else if(a==0)
+{
+System.out.println("zero");
+}
+else
+{
+System.out.println("pos");
+}
+}
+}
